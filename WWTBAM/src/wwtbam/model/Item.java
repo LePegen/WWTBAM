f0941f6aa93fe8289df05605840540472336a52f
LePegen <lipatjobj@gmail.com>
2018-10-02 22:20:10 +0800

    initial commit

--- WWTBAM/src/wwtbam/model/Item.java
+++ WWTBAM/src/wwtbam/model/Item.java
@@ -14,8 +14,40 @@ import java.util.ArrayList;
 public class Item {
    
     private String question;
-    private boolean isCorrect=false;
     private ArrayList<AbstractAnswer> choices;
+    private boolean isCorrect=false;
+
+    public Item(String question) {
+        this.question = question;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public ArrayList<AbstractAnswer> getChoices() {
+        return choices;
+    }
+
+    public void setChoices(ArrayList<AbstractAnswer> choices) {
+        this.choices = choices;
+    }
+
+    public boolean isIsCorrect() {
+        return isCorrect;
+    }
+
+    public void setIsCorrect(boolean isCorrect) {
+        this.isCorrect = isCorrect;
+    }
+    
+    
+    
+    
     
     
     
--- WWTBAM/src/wwtbam/model/ItemFactory.java
+++ WWTBAM/src/wwtbam/model/ItemFactory.java
@@ -5,10 +5,71 @@
  */
 package wwtbam.model;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import org.json.*;
+
 /**
  *
  * @author lipat
  */
 public class ItemFactory {
+
+    File file;
+    JSONObject jObject;
+    ArrayList<JSONObject> items;
+    int index=0;
     
+    //redo code
+    public ItemFactory() {
+        //breaks SRP. suggestion: move to other class
+        this.file = new File("C:\\Users\\lipat\\Documents\\School\\WWTBAM\\WWTBAM\\WWTBAM\\src\\wwtbam\\model\\choices.json");
+        FileReader fileReader;
+        BufferedReader bufferedReader;
+        try {
+            fileReader = new FileReader(file);
+            bufferedReader = new BufferedReader(fileReader);
+            String jsonContent = "";
+            String tempString = "";
+            while (!"".equals(tempString = bufferedReader.readLine())) {
+                jsonContent += tempString;
+            }
+       
+            this.jObject = new JSONObject(jsonContent);
+         
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        items = new ArrayList<>();
+        addItems();
+    }
+
+    public void addItems() {
+        
+        JSONObject tempObject;
+        int i = 0;
+        for (int j = 0; j < jObject.getJSONArray("Questions").length(); j++) {
+            tempObject = jObject.getJSONArray("Questions").getJSONObject(i);
+            items.add(tempObject);
+            
+        }
+    }
+    
+    public Item getItem(){
+        String question=items.get(index).getString("Question");
+        return new Item(question);
+    
+    }
+
+    public static void main(String[] args) {
+        ItemFactory itemFactory=new ItemFactory();
+       
+               
+        System.out.println(itemFactory.getItem().getQuestion());
+    }
+
 }
